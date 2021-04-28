a56ce060d5670858e80346bac81648cfaa42ab91
Marc <marcojuhel@gmail.com>
2021-04-28 20:50:57 +0200

    Oups

--- src/fr/marc/pokemon/Attaques.java
+++ src/fr/marc/pokemon/Attaques.java
@@ -10,47 +10,7 @@ public class Attaques {
 	private int dmgAttaque;
 	private String element;
 
-	public Attaques() {
 
-	}
-
-	public Attaques(String nomAttaque, int dmgAttaque, String element) {
-		this.nomAttaque = nomAttaque;
-		this.dmgAttaque = dmgAttaque;
-		this.element = element;
-
-	}
-
-	public int getDmgAttaque() {
-		return dmgAttaque;
-	}
-
-	public void setDmgAttaque(int dmgAttaque) {
-		this.dmgAttaque = dmgAttaque;
-	}
-
-	public String getNomAttaque() {
-		return nomAttaque;
-	}
-
-	public void setNomAttaque(String nomAttaque) {
-		this.nomAttaque = nomAttaque;
-	}
-
-	public String getElement() {
-		return element;
-	}
-
-	public void setElement(String element) {
-		this.element = element;
-	}
-
-	public static List<Attaques> ATTAQUE_GAYLORD = Arrays.asList(new Attaques("Giflette", 5, null),
-			new Attaques("Coup de tête", 10, null), new Attaques("Crachat", 7, "eau"),
-			new Attaques("Coup de point", 6, "vent"), new Attaques("Coup special : ghost rider", 25, "feu"));
-	public static List<Attaques> ATTAQUE_ADRIEN = Arrays.asList(new Attaques("Tacle par derrière", 8, null),
-			new Attaques("Double gifle papillon", 12, "vent"), new Attaques("Drague lourde", 5, "tenebres"),
-			new Attaques("Regard insistant", 4, "tenebres"), new Attaques("Coup special : Nazaret big wave", 24, "eau")
 
 	);
 	public static List<Attaques> ATTAQUE_ADIL = Arrays.asList(new Attaques("Roulade", 2, null),
